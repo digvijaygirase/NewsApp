1ef44f6a93cee0dab0f2cbfa47f695ff5534ba18
digvijaygirase <digvijay.girase57@gmail.com>
2022-03-13 17:15:14 +0530

    Firebase analytics integration and events logging

--- app/src/main/java/com/digvijay/newsapp/presentation/article/NewsArticleFragment.kt
+++ app/src/main/java/com/digvijay/newsapp/presentation/article/NewsArticleFragment.kt
@@ -1,12 +1,9 @@
 package com.digvijay.newsapp.presentation.article
 
-import android.os.Build
 import android.os.Bundle
-import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
@@ -16,11 +13,20 @@ import com.digvijay.newsapp.databinding.FragmentNewsArticleBinding
 import com.digvijay.newsapp.presentation.headlines.HeadlinesViewModel
 import com.digvijay.newsapp.util.setDate
 import com.digvijay.newsapp.util.setHtmlText
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
+
 
 class NewsArticleFragment : Fragment() {
 
     private lateinit var binding: FragmentNewsArticleBinding
     private val viewModel: HeadlinesViewModel by activityViewModels()
+
+    private val firebaseAnalytics by lazy {
+        Firebase.analytics
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,16 +39,18 @@ class NewsArticleFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        logTrackScreenEvent()
         val args: NewsArticleFragmentArgs by navArgs()
         viewModel.headlinesResource.value?.run {
             data?.get(args.position)?.run {
+                logNewsClickEvent(title ?: "")
                 if (!author.isNullOrEmpty()) {
                     binding.authorNameTV.setHtmlText(author)
                 } else {
                     binding.authorNameTV.text = getString(R.string.unknown)
                 }
                 binding.titleTV.text = title
-                binding.publishedDateTV.setDate(publishedAt?:"")
+                binding.publishedDateTV.setDate(publishedAt ?: "")
                 binding.descriptionTV.text = description
                 binding.contentTV.text = content
                 Glide.with(binding.root)
@@ -50,6 +58,19 @@ class NewsArticleFragment : Fragment() {
                     .placeholder(R.drawable.no_image)
                     .into(binding.newsImageIV)
             }
+        }
+    }
+
+    private fun logNewsClickEvent(newsTitle: String) {
+        firebaseAnalytics.logEvent("News click") {
+            param("news_title", newsTitle)
+        }
+    }
+
+    private fun logTrackScreenEvent() {
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            param(FirebaseAnalytics.Param.SCREEN_NAME, "News Article")
+            param(FirebaseAnalytics.Param.SCREEN_CLASS, "NewsArticleFragment")
         }
     }
 }
